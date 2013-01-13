957550da820cdd22458b353223512ff832372acf
Kevin Ewing <notmyuwmail@gmail.com>
2013-01-12 21:40:37 -0800

    Initial commit (working)

---
+++ js/main.js
@@ -0,0 +1,99 @@
+(function() {
+
+  window.K = {
+    init: function() {
+      R.ready(K._initUserButton);
+
+      $('#reset').bind('click', function() {
+        $('.confirmdisplay .playlistbutton').removeClass('chosen').appendTo('.userplaylists');
+        $('#proceed').hide();
+      });
+
+      $('#proceed').bind('click', function() {
+        K.getChosen();
+        $('.combinedetails').show();
+      });
+
+      $('#cancel').bind('click', function(e) {
+        e.preventDefault();
+        $('.combinedetails').hide();
+        $('#pldesc textarea').attr('value', '');
+        K.playlister.newListKeys = [];
+      });
+
+      $('#merge').bind('click', function(e) {
+        e.preventDefault();
+        K.playlister.makeNewPlaylist();
+      })
+
+      $('.aboutbutton').bind('click', function() {
+        $('#about').show();
+      });
+
+      $('#about').bind('click', function() {
+        $(this).hide();
+      });
+    },
+
+    _initUserButton: function() {
+      if (R.authenticated()) {
+        $('.greeting').remove();
+        K.playlister.getUserPlaylists();
+        var displayName = R.currentUser.get('vanityName');
+        $('#rdiobutton')
+          .attr('value', 'Logged in as: ' + displayName)
+          .bind('click', function() {
+            window.open("http://www.rdio.com/people/"+ displayName);
+        }); 
+      } else {
+        $('#rdiobutton')
+          .attr('value', 'Click Here To Authorize/Log In With Rdio')
+          .bind('click', function() {
+            R.authenticate(K._initUserButton);
+        });
+      }
+    },
+
+    toggleChosen: function(button) {
+      var $button = $(button);
+
+      if ($button.hasClass('chosen')) {
+        $button.toggleClass('chosen').appendTo('.userplaylists');
+      } else {
+        $button.toggleClass('chosen').appendTo('.confirmdisplay');
+      }
+      
+      var num = $('.chosen').length;
+      if (num >= 2) {
+        $('#proceed').show();
+      } else {
+        $('#proceed').hide();
+      }
+    },
+
+    getChosen: function() {
+      var $choices = $('.chosen');
+      var ph = 'Combination of: ';
+
+      $.each($choices, function(i, v) {
+        ph += v.innerHTML + ' + ';
+        _.each(K.playlister.rdioPlaylists, function(v2, i2) {
+            if (v2.name == v.innerHTML) {
+              K.addChosen(v2.trackKeys);
+            }
+        });
+      });
+      ph = ph.slice(0, ph.length - 3);
+      $('#pldesc textarea').attr('value', ph);
+    },
+
+    addChosen: function(keyArray) {
+      K.playlister.newListKeys= K.playlister.newListKeys.concat(keyArray);
+    }
+  };
+  // +++++++++++++++++++
+  $(document).ready(function() {
+    K.init();
+  });
+
+})();
---
+++ js/playlister.js
@@ -0,0 +1,59 @@
+(function() {
+
+	K.playlister = {
+		rdioPlaylists: [],
+		newListKeys: [],
+
+		getUserPlaylists: function() {
+		    var self = this;
+
+		    R.ready(function() {
+			    R.request({
+			        method:"getUserPlaylists",
+			        content:{
+			          user: R.currentUser.get("key"),
+			          count: 100,
+			          extras: '-*,name,key,trackKeys'
+			        },
+			        success: function(data) {
+			          _.each(data.result, function(v, i) {
+			            K.playlister.rdioPlaylists.push(v);
+			            K.playlister.makePlaylistMenuButton(v);
+			          });
+			        }
+			    });
+		    });
+		},
+
+		makePlaylistMenuButton: function(playlist) {
+			var $pl = $('<div />', {
+		      'class': 'playlistbutton',
+		      'id': playlist.key,
+		      html: playlist.name
+		      }).bind('click', function() {
+		      	K.toggleChosen(this);
+		      }).appendTo('.userplaylists');
+		      
+		    $('.confirmbox').show();
+		},
+
+		makeNewPlaylist: function() {
+			plName = $('#plname input').attr('value');
+			plDesc = $('#pldesc textarea').attr('value');
+
+			R.request({
+				method: 'createPlaylist',
+				content: {
+					name: plName,
+					description: plDesc,
+					tracks: K.playlister.newListKeys.join()
+				},
+				success: function() {
+			        $('.combinedetails').hide();
+			        $('.confirmdisplay .playlistbutton').removeClass('chosen').appendTo('.userplaylists');
+				}
+			});
+		}
+	};
+
+})();
