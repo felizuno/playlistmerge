4e86dbe5e9aed993cf1f009145ba249954e8a6f0
Kevin Ewing <notmyuwmail@gmail.com>
2013-01-17 21:50:29 -0800

    tabs

--- js/playlister.js
+++ js/playlister.js
@@ -1,60 +1,60 @@
 (function() {
 
-	K.playlister = {
-		rdioPlaylists: [],
-		newListKeys: [],
+  K.playlister = {
+    rdioPlaylists: [],
+    newListKeys: [],
 
-		getUserPlaylists: function() {
-		    R.ready(function() {
-			    R.request({
-			        method:"getUserPlaylists",
-			        content:{
-			          user: R.currentUser.get("key"),
-			          count: 100,
-			          extras: '-*,name,key,trackKeys'
-			        },
-			        success: function(data) {
-			          _.each(data.result, function(v, i) {
-			            K.playlister.rdioPlaylists.push(v);
-			            K.playlister.makePlaylistMenuButton(v);
-			          });
-			        }
-			    });
-		    });
-		},
+    getUserPlaylists: function() {
+      R.ready(function() {
+          R.request({
+            method:"getUserPlaylists",
+            content:{
+              user: R.currentUser.get("key"),
+              count: 100,
+              extras: '-*,name,key,trackKeys'
+            },
+            success: function(data) {
+              _.each(data.result, function(v, i) {
+                K.playlister.rdioPlaylists.push(v);
+                K.playlister.makePlaylistMenuButton(v);
+              });
+            }
+          });
+      });
+    },
 
-		makePlaylistMenuButton: function(playlist) {
-			var $pl = $('<div />', {
-		      'class': 'playlistbutton',
-		      'id': playlist.key,
-		      html: playlist.name
-		      }).bind('click', function() {
-		      	K.toggleChosen(this);
-		      }).appendTo('.userplaylists');
+    makePlaylistMenuButton: function(playlist) {
+      var $pl = $('<div />', {
+        'class': 'playlistbutton',
+        'id': playlist.key,
+        html: playlist.name
+        }).bind('click', function() {
+          K.toggleChosen(this);
+        }).appendTo('.userplaylists');
 
-		    $('.confirmbox').show();
-		},
+      $('.confirmbox').show();
+    },
 
-		makeNewPlaylist: function() {
-			var plName = $('#plname input').attr('value');
-			var plDesc = $('#pldesc textarea').attr('value');
-	        $('#newlist').html(plName);
+    makeNewPlaylist: function() {
+      var plName = $('#plname input').attr('value');
+      var plDesc = $('#pldesc textarea').attr('value');
+      $('#newlist').html(plName);
 
-			R.request({
-				method: 'createPlaylist',
-				content: {
-					name: plName,
-					description: plDesc,
-					tracks: K.playlister.newListKeys.join()
-				},
-				success: function() {
-			        $('.playlistbutton').not('#newlist').remove();
-			        $('#sucessscreen').show()
-			        K.playlister.getUserPlaylists();
-			        $('.combinedetails').hide();
-				}
-			});
-		}
-	};
+      R.request({
+        method: 'createPlaylist',
+        content: {
+          name: plName,
+          description: plDesc,
+          tracks: K.playlister.newListKeys.join()
+        },
+        success: function() {
+          $('.playlistbutton').not('#newlist').remove();
+          $('#sucessscreen').show()
+          K.playlister.getUserPlaylists();
+          $('.combinedetails').hide();
+        }
+      });
+    }
+  };
 
 })();
